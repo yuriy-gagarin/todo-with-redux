d2bab94c487d396a2ec5ba879d559c56fd57fc71
deloff <patch419@gmail.com>
2018-12-16 20:31:19 +0300

    scroller

--- src/views/Slider.js
+++ src/views/Slider.js
@@ -1,27 +1,50 @@
 import React, { useRef, useEffect, useState } from 'react'
+import { debounce } from 'lodash'
 
 const _arrowKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
 
 const Slider = (props) => {
   const sliderRef = useRef()
-  const [current, setCurrent] = useState(0)
-
-  const handleKeyDown = event => {
-    if (_arrowKeys.includes(event.key)) {
-      event.preventDefault()
-    }
-  }
 
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown)
-    return () =>
-      document.removeEventListener('keydown', handleKeyDown)
+    document.addEventListener('keydown', onKeydown)
+    return () => document.removeEventListener('keydown', onKeydown)
   }, [])
+  
+  const onKeydown = event => {
+    if (!_arrowKeys.includes(event.key)) return
+    event.preventDefault()
+    if (event.key === 'ArrowLeft') {
+      sliderRef.current.scrollLeft = 0
+      return
+    }
+    if (event.key === 'ArrowRight') {
+      sliderRef.current.scrollLeft = window.innerWidth + 10
+      return
+    }
+  }
+  
+  const onScroll = event => {
+    const current = Math.floor(sliderRef.current.scrollLeft / (window.innerWidth + 10))
+    console.log(current)
+  }
+
+  const onWheel = event => {
+    event.preventDefault()
+    const delta = Math.abs(event.deltaY) > Math.abs(event.deltaX) ? event.deltaY : event.deltaX
+    if (delta >= 100) {
+      sliderRef.current.scrollLeft = window.innerWidth + 10
+    }
+    sliderRef.current.scrollLeft = sliderRef.current.scrollLeft + delta
+  }
 
   return (
     <div 
       className='Slider'
       ref={sliderRef}
+      onScroll={onScroll}
+      onWheel={onWheel}
+      // onKeydown={onKeydown}
     >
       {props.children}
     </div>
